6af7e76f2b83ef2ded26697c3640e071c1a8b603
Marcel de Bont <86848142+marceldbo@users.noreply.github.com>
2026-07-04 19:46:12 +0200

    Update devices.py

--- indevolt/devices.py
+++ indevolt/devices.py
@@ -1,5 +1,5 @@
 import Domoticz
-from indevolt.constants import CHARGING_STATE_MAP
+from indevolt.constants import CHARGING_STATE_MAP, WORKING_MODE_MAP
 
 class DeviceManager:
 
@@ -113,11 +113,27 @@ class DeviceManager:
                         sValue=state_text
                     )
                     continue
+                    
+                # -----------------------------
+                # WORKING MODE (7101)
+                # -----------------------------
+                if tag == "7101":
+                    raw_state = int(value)
+                    state_text = WORKING_MODE_MAP.get(
+                        raw_state,
+                        f"Unknown ({raw_state})"
+                    )
 
+                    self.Devices[unit].Update(
+                        nValue=0,
+                        sValue=state_text
+                    )
+                    continue 
+                    
                 # -----------------------------
                 # TEXT DEVICES
                 # -----------------------------
-                if unit in (1, 2):
+                if unit in (1):
                     self.Devices[unit].Update(
                         nValue=0,
                         sValue=str(value)
