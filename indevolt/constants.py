be64c9a4a151f70096ecca761a5e93023b8b35ea
Marcel de Bont <86848142+marceldbo@users.noreply.github.com>
2026-07-10 14:51:35 +0200

    Rename power tags to AC input/output power

--- indevolt/constants.py
+++ indevolt/constants.py
@@ -16,8 +16,8 @@ TAG_BATTERY_POWER = 6000
 TAG_CHARGING_STATE = 6001
 TAG_BATTERY_SOC = 6002
 
-TAG_TOTAL_INPUT_POWER = 2101
-TAG_TOTAL_OUTPUT_POWER = 2108
+TAG_TOTAL_AC_INPUT_POWER = 2101
+TAG_TOTAL_AC_OUTPUT_POWER = 2108
 
 TAG_TOTAL_INPUT_ENERGY = 2107
 TAG_TOTAL_OUTPUT_ENERGY = 2104
@@ -93,8 +93,8 @@ POLL_TAGS = [
     TAG_BATTERY_POWER,
     TAG_BATTERY_SOC,
 
-    TAG_TOTAL_INPUT_POWER,
-    TAG_TOTAL_OUTPUT_POWER,
+    TAG_TOTAL_AC_INPUT_POWER,
+    TAG_TOTAL_AC_OUTPUT_POWER,
 
     TAG_TOTAL_INPUT_ENERGY,
     TAG_TOTAL_OUTPUT_ENERGY,
@@ -207,23 +207,31 @@ DEVICE_DEFINITIONS = {
     },
 
 
-    TAG_TOTAL_INPUT_POWER: {
+    TAG_TOTAL_AC_INPUT_POWER: {
         "unit": 6,
-        "name": "Grid Input Power",
+        "name": "Total AC Input Power",
+        "custom_unit": "W",
         "create": {
             "Type": 243,
-            "Subtype": 29,
+            "Subtype": 31,
+            "Options": {
+                "Custom": "1;W",
+            },
             "Used": 1,
         },
     },
 
 
-    TAG_TOTAL_OUTPUT_POWER: {
+    TAG_TOTAL_AC_OUTPUT_POWER: {
         "unit": 7,
-        "name": "Grid Output Power",
+        "name": "Total AC Output Power",
+        "custom_unit": "W",
         "create": {
             "Type": 243,
-            "Subtype": 29,
+            "Subtype": 31,
+            "Options": {
+                "Custom": "1;W",
+            },
             "Used": 1,
         },
     },
