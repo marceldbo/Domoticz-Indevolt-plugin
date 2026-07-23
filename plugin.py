0d976a64b7ed8fd8e5729fa8d74313e5789d3dc2
Marcel de Bont <86848142+marceldbo@users.noreply.github.com>
2026-07-23 15:30:04 +0200

    Fix indentation and formatting in plugin.py

--- plugin.py
+++ plugin.py
@@ -25,8 +25,7 @@ Version 2.1.0
         <param field="Address"
                label="Indevolt IP"
                width="200px"
-               required="true"
-               default="192.168.10.142"/>
+               required="true"/>
 
         <param field="Port"
                label="Port"
