ba5f750bc3e5fcc024118580dbceb8c5a5b67daa
Marcel de Bont <86848142+marceldbo@users.noreply.github.com>
2026-07-28 13:45:11 +0200

    Update WORKING_MODE_MAP for clarity

--- indevolt/constants.py
+++ indevolt/constants.py
@@ -72,7 +72,7 @@ SET_GRID_CHARGING_ENABLE = 1143
 # ==========================================================
 
 WORKING_MODE_MAP = {
-    1: "Self-consumed Prioritized",
+    1: "Self-consumption",
     4: "Real-time Control",
     5: "Charge/Discharge Schedule",
 }
