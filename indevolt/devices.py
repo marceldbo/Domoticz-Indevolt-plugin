969e8449bca3ac0e0af6ddc8069f116e1c14a10a
Marcel de Bont <86848142+marceldbo@users.noreply.github.com>
2026-09-02 17:58:06 +0200

    Slight modification to log output

--- indevolt/devices.py
+++ indevolt/devices.py
@@ -283,8 +283,8 @@ class DeviceManager:
     
                     log_debug(
                         f"Total AC Input: "
-                        f"{input_power} W, "
-                        f"{total_input_energy:.2f} kWh, "
+                        f"{input_power} W | "
+                        f"{total_input_energy:.2f} kWh | "
                         f"sValue={svalue}"
                     )
     
@@ -336,8 +336,8 @@ class DeviceManager:
     
                     log_debug(
                         f"Total AC Output: "
-                        f"{output_power} W, "
-                        f"{total_output_energy:.2f} kWh, "
+                        f"{output_power} W | "
+                        f"{total_output_energy:.2f} kWh | "
                         f"sValue={svalue}"
                     )
     
